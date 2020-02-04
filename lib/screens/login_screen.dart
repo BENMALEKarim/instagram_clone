2e0afe99063839a30af4bb5bfef3dad50eddcfcb
BENMALEKarim <fk_benmalek@esi.dz>
2020-02-04 17:02:12 +0100

    login screen (form) : not completed

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
+  String _email, _password;
+
+  _submit(){
+    
+  }
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Instagram',
+              style: TextStyle(fontSize: 50, fontFamily: 'Billabong'),
+            ),
+            Form(
+              key: _formKey,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Padding(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 30.0, vertical: 10.0),
+                    child: TextFormField(
+                      decoration: InputDecoration(labelText: 'Email'),
+                      validator: (input) => !input.contains('@')
+                          ? 'Please enter a valid email'
+                          : null,
+                      onSaved: (input) => _email = input,
+                    ),
+                  ),
+                  Padding(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 30.0, vertical: 10.0),
+                    child: TextFormField(
+                      decoration: InputDecoration(labelText: 'Password'),
+                      validator: (input) => input.length < 6
+                          ? 'Password must be at least 6 characters'
+                          : null,
+                      onSaved: (input) => _password = input,
+                      obscureText: true,
+                    ),
+                  ),
+                  SizedBox(height: 20.0),
+                  Container(
+                    width: 250.0,
+                    child: FlatButton(
+                      onPressed: _submit,
+                      textColor: Colors.blue,
+                      padding: EdgeInsets.all(10.0),
+                      child: Text(
+                        'Login',
+                        style: TextStyle(color: Colors.white, fontSize: 18.0),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
