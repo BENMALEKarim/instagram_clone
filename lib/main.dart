bef989d0942fb8723452eef2a67281012604be5c
BENMALEKarim <fk_benmalek@esi.dz>
2020-03-14 01:20:13 +0100

    firebase env preparation step1

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,27 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/screens/login_screen.dart';
 import 'package:instagram_clone/screens/signup_screen.dart';
+import 'package:instagram_clone/screens/feed_screen.dart';
+
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  // This widget is the root of your application
+    Widget _getScreenId() {
+    return StreamBuilder<FirebaseUser>(
+      stream: FirebaseAuth.instance.onAuthStateChanged,
+      builder: (BuildContext context, snapshot) {
+        if (snapshot.hasData) {
+          return FeedScreen();
+        } else {
+          return LoginScreen();
+        }
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
