ec6f1216190f236304b26f26cc43c324f7cfbc8e
BENMALEKarim <fk_benmalek@esi.dz>
2020-03-15 19:09:46 +0100

    user static profile

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -87,11 +87,37 @@ class _ProfileScreenState extends State<ProfileScreen> {
                             textColor: Colors.white,
                             child: Text(
                               'Edit Profile',
-                              style: TextStyle(fontSize: 18.0,),
+                              style: TextStyle(
+                                fontSize: 18.0,
+                              ),
                             ),
                           ),
                         ),
                       ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 10.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    'Username',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 5.0),
+
+                  Container(
+                    width: 80.0,
+                    child: Text(
+                      'bio',
+                      style: TextStyle(color: Colors.black54, fontSize: 15.0),
                     ),
                   ),
                 ],
