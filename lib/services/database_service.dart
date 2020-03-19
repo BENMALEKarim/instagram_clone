56de85fa0c8cbfdd142e41bb64f19329eb0b9c17
BENMALEKarim <fk_benmalek@esi.dz>
2020-03-19 20:59:03 +0100

    Adding search user

--- lib/services/database_service.dart
+++ lib/services/database_service.dart
@@ -1,14 +1,19 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:instagram_clone/models/user_model.dart';
 import 'package:instagram_clone/utiities/constants.dart';
 
-class DatabaseService{
-
-  static void updateUser(User user){
+class DatabaseService {
+  static void updateUser(User user) {
     userRef.document(user.id).updateData({
       'name': user.name,
-      'bio' : user.bio,
+      'bio': user.bio,
       'profileImageUrl': user.profileImageUrl,
     });
   }
 
+  static Future<QuerySnapshot> searchUser(String name) {
+    Future<QuerySnapshot> users =
+        userRef.where('name', isGreaterThanOrEqualTo: name).getDocuments();
+        return users;
+  }
 }
