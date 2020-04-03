d14127d892e1b8982af27cef2fed25e848791420
BENMALEKarim <fk_benmalek@esi.dz>
2020-04-03 19:48:37 +0100

    finish the app with all features

--- lib/utiities/constants.dart
+++ lib/utiities/constants.dart
@@ -7,3 +7,9 @@ final usersRef = _firestore.collection('users');
 final postsRef = _firestore.collection('posts');
 final followersRef = _firestore.collection('followers');
 final followingRef = _firestore.collection('following');
+
+final feedsRef = _firestore.collection('feeds');
+
+final likesRef = _firestore.collection('likes');
+final commentsRef = _firestore.collection('comments');
+final activitiesRef = _firestore.collection('activities');
